7ca51dda69a4b646504c3de30c345bfb1e824c4f
Thiago Raia de Moura <thiagoraia2004@gmail.com>
2026-04-10 16:52:15 -0300

    adicao de objetivos na etapa

--- src/objetivo/objetivo.service.ts
+++ src/objetivo/objetivo.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateObjetivoDto } from './dto/create-objetivo.dto';
 import { UpdateObjetivoDto } from './dto/update-objetivo.dto';
+import { Objetivo } from './entities/objetivo.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class ObjetivoService {
+  constructor(
+    @InjectRepository(Objetivo)
+    private objetivoRepository: Repository<Objetivo>,
+  ) {}
+
   create(createObjetivoDto: CreateObjetivoDto) {
     return 'This action adds a new objetivo';
   }
@@ -16,8 +24,19 @@ export class ObjetivoService {
     return `This action returns a #${id} objetivo`;
   }
 
-  update(id: number, updateObjetivoDto: UpdateObjetivoDto) {
-    return `This action updates a #${id} objetivo`;
+  async update(id: number, updateObjetivoDto: UpdateObjetivoDto) {
+    const objetivo = await this.objetivoRepository.findOne({ where: { id } });
+
+    if (!objetivo) {
+      throw new NotFoundException('Objetivo não encontrado.');
+    }
+
+    // Mescla os dados antigos com os novos
+    Object.assign(objetivo, updateObjetivoDto);
+
+    const objetivoAtualizado = await this.objetivoRepository.save(objetivo);
+
+    return objetivoAtualizado;
   }
 
   remove(id: number) {
